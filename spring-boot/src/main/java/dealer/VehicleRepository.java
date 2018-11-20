85594f40c1db1cfdb11441203472bc420b835e99
Kuei-chun Chen <simagix@gmail.com>
2018-11-20 10:11:42 -0500

    Add more examples

--- spring-boot/src/main/java/dealer/VehicleRepository.java
+++ spring-boot/src/main/java/dealer/VehicleRepository.java
@@ -2,12 +2,20 @@ package dealer;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface VehicleRepository extends MongoRepository<Vehicle, String> {
 
     public List<Vehicle> findByColor(String color);
     public List<Vehicle> findByStyle(String style);
     public List<Vehicle> findByColorAndStyle(String color, String style);
+    
+    @Query("{ color : 'Red', style: 'Truck' }")
+    public List<Vehicle> findRedTrucks(Pageable pageable);
+    
+    @Query("{ color : 'Red', isNew: true }")
+    public List<Vehicle> findNewRedVehicle(Pageable pageable);
 
 }
